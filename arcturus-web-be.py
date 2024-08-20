23758b8457ed3bf4867387243d4a1731e2951ca6
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-08-20 08:30:48 -0300

    feat: First commit

---
+++ arcturus-web-be.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/api/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            # Example JSON response
+            response = {"message": "Hello from the backend!"}
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
