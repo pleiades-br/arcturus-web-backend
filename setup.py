050c6a821488f045ab94ccc0d21f56df432bae7b
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-08-20 08:40:56 -0300

    feat: addint setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+    setup tools
+"""
+from setuptools import setup
+setup(
+    name = "arcturus-web-be",
+    version = "0.1",
+    packages=["arcturus-web-be"],
+    author="Aluisio Leonello Victal",
+    author_email = "alvictal@gmail.com",
+    description = "Backend for project arcturus",
+    license = "MIT",
+    keywords= "Backend",
+    url = "",
+)
