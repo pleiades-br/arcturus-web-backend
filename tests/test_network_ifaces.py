dc238c40f5ca2f3745ad5b9a6d7f4ce6472357cd
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-08-27 02:00:18 +0000

    feat: Adding more tests to network_ifaces module

--- tests/test_network_ifaces.py
+++ tests/test_network_ifaces.py
@@ -12,10 +12,12 @@ sys.path.append(SOURCE_PATH)
 import network_ifaces as netif 
 
 eth_iface = netif.EthernetIface("eth0")
+print(eth_iface.config_ethertnet(ipaddr="10.10.0.1"))
 print(eth_iface.get_interface_info())
 
 wifi_iface = netif.WiFiIface("enps01")
 print(wifi_iface.get_interface_info())
 
 LTE_iface = netif.LTEIface("ppp0")
+print(LTE_iface.config_lte("vivo.co.br"))
 print(LTE_iface.get_interface_info())
