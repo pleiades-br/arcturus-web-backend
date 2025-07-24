a92ab0249ae21e211735ff134d31e2a3f73863c8
Marcella <marques.marcella@gmail.com>
2025-07-24 16:47:07 +0100

    adjusted get & post method for mqtt

--- be_service/mqtt_data.py
+++ be_service/mqtt_data.py
@@ -3,17 +3,30 @@ import logging
 
 MQTT_DATA_FILE = "/tmp/safetyrails/mqtt"
 
+
 def get_mqtt_data():
     mqtt_data = None
     try:
         with open(MQTT_DATA_FILE, 'r') as mqtt_file:
             mqtt_data = json.load(mqtt_file)
     except Exception as e:
-        logging.error(f'Not possible to get data from file: {SENSOR_DATA_FILE}. Error: {e}')
+        logging.error(f'Not possible to get data from file: \
+                      {MQTT_DATA_FILE}. Error: {e}')
 
     if mqtt_data is not None:
         mqtt_data["status"] = 200
         return mqtt_data
 
-
     return {'status': 400}
+
+
+def post_ethernet_config(mqtt_config):
+    try:
+        with open(MQTT_DATA_FILE, 'w') as mqtt_file:
+            json.dump(mqtt_config, mqtt_file)
+    except Exception as e:
+        logging.error(f'Not possible to save data on file: \
+                      {MQTT_DATA_FILE}. Error: {e}')
+        return {'status': 400}
+
+    return {'status': 200}
